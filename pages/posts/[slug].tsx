408af30c29722098c8c072058379f34afbf6f15b
mikias-tulu <miki.tulu@gmail.com>
2024-03-23 17:09:27 +0300

    deployment fix: import error

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,26 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
-import { allPosts } from 'contentlayer/generated'
+
 import { NextPage } from 'next'
 
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
 export async function getStaticPaths() {
-  const paths = allPosts.map((post) => post.url)
-  return {
-    paths,
-    fallback: false,
-  }
+  
+  return <></>
 }
 
 export async function getStaticProps({ params }) {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
-  return {
-    props: {
-      post,
-    },
-  }
+  
+  return <></>
 }
 
 const PostLayout: NextPage = ({ post }: any) => {
